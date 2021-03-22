f5f21a77a5de40de5815adb643285901cf3e32f8
dotnet290121 <78213874+dotnet290121@users.noreply.github.com>
2021-03-23 00:19:19 +0200

    Add files via upload

---
+++ WaitHandlers2203/WaitHandlers2203/PageLoader.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace WaitHandlers2203
+{
+    public class PageLoader
+    {
+
+        private ManualResetEvent gate = new ManualResetEvent(false);
+
+        public void MakeWindowAnimation()
+        {
+            Console.WriteLine("MakeWindowAnimation reached gate");
+            // wait for page to be loaded
+            gate.WaitOne();
+            Console.WriteLine("Drawing page animation ...");
+
+        }
+        public void ShowAmazingScrollBarsAnimation()
+        {
+            Console.WriteLine("ShowAmazingScrollBarsAnimation reached gate");
+            // wait for page to be loaded
+            gate.WaitOne();
+            Console.WriteLine("ShowAmazingScrollBarsanimation ...");
+
+        }
+        public void FireRequestToWebSite()
+        {
+            Console.WriteLine("FireRequestToWebSite reached gate");
+            // wait for page to be loaded
+            gate.WaitOne();
+            Console.WriteLine("FireRequestTo WebSite ...");
+
+        }
+        public void LoadPage()
+        {
+            Console.WriteLine("Loading page...");
+            Thread.Sleep(5 * 1000);
+            Console.WriteLine("Page finished loading");
+
+            gate.Set();
+        }
+    }
+}
---
+++ WaitHandlers2203/WaitHandlers2203/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WaitHandlers2203
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ReadLine();
+
+            PageLoader page = new PageLoader();
+            Task t1 = Task.Run(() => { page.MakeWindowAnimation(); });
+            Task t2 = Task.Run(() => { page.ShowAmazingScrollBarsAnimation(); });
+            Task t3 = Task.Run(() => { page.FireRequestToWebSite(); });
+            Task t4 = Task.Run(() => { page.LoadPage(); });
+
+            Task.WaitAll(new Task[] { t1, t2, t3, t4 });
+            Console.ReadLine();
+        }
+    }
+}
